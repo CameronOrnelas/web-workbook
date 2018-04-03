814e022f9d73b105ea750c2e25062cd4b81a7105
Cameron Ornelas <35507124+CameronOrnelas@users.noreply.github.com>
2018-04-02 21:40:51 -0500

    Finished

--- 08week/tic-tac-toe/script.js
+++ 08week/tic-tac-toe/script.js
@@ -1,5 +1,51 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+  let turn = 'X';
+  $('[data-cell]').on('click', function(){
+      $(this).text(turn);
+      wins();
+      if(turn === 'X') {
+        turn = 'O';
+      }else {
+        turn = 'X';
+      }
+});
+
+  function wins() {
+    if(
+      ($('[date-cell="0"]').text() === turn &&
+      $('[date-cell="3"]').text() === turn &&
+      $('[date-cell="6"]').text() === turn) ||
+      ($('[date-cell="1"]').text() === turn &&
+      $('[date-cell="4"]').text() === turn &&
+      $('[date-cell="7"]').text() === turn) ||
+      ($('[date-cell="2"]').text() === turn &&
+      $('[date-cell="5"]').text() === turn &&
+      $('[date-cell="8"]').text() === turn) ||
+      ($('[date-cell="0"]').text() === turn &&
+      $('[date-cell="1"]').text() === turn &&
+      $('[date-cell="2"]').text() === turn) ||
+      ($('[date-cell="3"]').text() === turn &&
+      $('[date-cell="4"]').text() === turn &&
+      $('[date-cell="5"]').text() === turn) ||
+      ($('[date-cell="6"]').text() === turn &&
+      $('[date-cell="7"]').text() === turn &&
+      $('[date-cell="8"]').text() === turn) ||
+      ($('[date-cell="0"]').text() === turn &&
+      $('[date-cell="4"]').text() === turn &&
+      $('[date-cell="8"]').text() === turn) ||
+      ($('[date-cell="2"]').text() === turn &&
+      $('[date-cell="4"]').text() === turn &&
+      $('[date-cell="6"]').text() === turn)
+    ) {
+  $('#announce-winner').text(`Player ${turn} Wins!`)
+    }
+  }
+
+  $('#clear').click(function() {
+    $('[data-cell], #announce-winner').text('');
+    turn = 'X';
+  });
+
 });
